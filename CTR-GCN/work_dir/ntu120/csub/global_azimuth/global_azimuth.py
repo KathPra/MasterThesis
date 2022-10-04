669d8c2f18ef3545ae0c61cf848ede73f4c38de5
K. Prasse <katharina.prasse@posteo.de>
2022-10-04 13:56:24 +0200

    standardize normalization

--- CTR-GCN/work_dir/ntu120/csub/global_azimuth/global_azimuth.py
+++ CTR-GCN/work_dir/ntu120/csub/global_azimuth/global_azimuth.py
@@ -126,53 +126,32 @@ class unit_gcn(nn.Module):
 class symmetry_module(nn.Module):
     def __init__(self):
         super(symmetry_module, self).__init__()
-
-    def local_coord(self,x):
-        N, C, T, V = x.size()
-
-        # new dim: 128 x 3 x 64 x 25 x 25
-        x = torch.stack([x] * V, axis=4) - torch.stack([x] * V, axis=3)
-        
-        return x
     
     def Spherical_coord(self,x):
+        eps = 0.0000001
+        azimuth = torch.atan2(torch.sqrt(x[:, 0]**2+ x[:, 1]**2+eps),x[:, 2]) # sqrt of neg value is a complex number -> NaN
+        if torch.isnan(torch.sqrt(x[:, 0]**2+ x[:, 1]**2)).any():
+            raise ValueError("nan in nominator")
+        if torch.isnan(x[:, 2]).any():
+            raise ValueError("nan in denominator")
+        if torch.isnan(azimuth).any():
+            raise ValueError("Nan in atan")
 
-        azimuth = torch.atan2(torch.sqrt(x[:, 0]**2+ x[:, 1]**2),x[:, 2])
+
         #colatitude = torch.atan2(x[:, 1], x[:, 0])
         #p = torch.sqrt(x[:, 0]**2 + x[:, 1]**2 + x[:, 2]**2) # magnitude of vector
         #x = torch.stack([p, colatitude, azimuth], dim =1)
         
         return azimuth.unsqueeze(1)
 
-    def Spherical_harm(self,x,l_range):
-        x_tran = x.cpu().detach()
-        result = None
-        #test1 = []
-        #torch.pi = torch.acos(torch.zeros(1)).item() * 2
-        for l in range(l_range+1):
-            m_range = np.arange(0,l+1,1, dtype=int)
-            for m in m_range:
-                test = scipy.special.sph_harm(m, l, x_tran[:,2],x_tran[:,1], out=None) # theta: azimuth, phi = colatitude
-                #raise ValueError(test.shape)
-                test = test.unsqueeze(1)
-                result = torch.cat((result, test),dim =1) if result is not None else test
-                # result: torch.Size([128, 21, 64, 25, 25]))
-        
-        # raise ValueError(test.shape, result.shape)
-        return result
-
-    def forward(self, x, l_range):
+   
+    def forward(self, x):
         N, C, T, _ = x.size()
 
         # convert from catesian coordinates to cylindrical
         #x = self.local_coord(x) # input [128,3,64,25], output [128, 64, 25]
         x = self.Spherical_coord(x)
-
-
-        #raise ValueError(x.shape)
-        N, C, T, _ = x.size()
-        x = x.view(N,C,T,-1)
-        
+        x = x.nan_to_num(nan=0.0)    
         
         #raise ValueError("test successful")
         return x
@@ -213,7 +192,7 @@ class Model(nn.Module):
         self.num_class = num_class
         self.num_point = num_point
         self.SHT = 1
-        self.data_bn = nn.BatchNorm1d(num_person * num_point  * self.SHT) # number of spherical harmonics, 2 * V because of lokal environment for each joint
+        self.data_bn = nn.BatchNorm1d(in_channels * self.num_point * num_person) # number of spherical harmonics, 2 * V because of lokal environment for each joint
 
         self.sym = symmetry_module()
         self.l1 = TCN_GCN_unit(self.SHT, 64, A, residual=False, adaptive=adaptive)
@@ -238,37 +217,26 @@ class Model(nn.Module):
     def forward(self, x):
         N, C, T, V, M = x.size()
 
-        # Prepare data for local SHT
-        x = x.permute(0, 4, 1, 2,3).contiguous().view(N * M, C, T, V)
+        # Prepare data for transform to spherical coord
+        # Code from original paper
+        
+        x = x.permute(0, 4, 3, 1, 2).contiguous().view(N, M * V * C, T)
+        # order is now N,(M,V,C),T -> print(x.shape) -> 64, 150, 64
+        x = self.data_bn(x)
+        #print(x.shape) -> shape stays the same
+        x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
+        # x is now 4 D: N*M, C, T,V
 
         # All skeletons should be normed, i.e. joint #1 should be on the origine. Not always the case -> corrected 
         x1 = torch.stack([x[:,:,:,1]]*V, dim = 3)
         x = x - x1
         x1 = None
 
-
         # send data to symmetry module
-        sym = self.sym(x, 2) # l_range
-        x = sym.cuda(x.get_device())
-        _, C, T, V = x.size()
+        x = self.sym(x) 
 
         #raise ValueError(x.shape) #-> 128, 1, 64, 25
-        
-        # Code from original paper
-        #raise ValueError(x.shape)
-        x = x.view(N,M,C,T,V).permute(0, 1, 4, 2, 3).contiguous().view(N, M * V * C, T)
-        
-        #raise ValueError(x.shape)
-        # order is now N,(M,V,C),T
-        #print(x.shape) -> 64, 150, 64
-        x = self.data_bn(x)
-        #print(x.shape) -> shape stays the same
-        x = x.view(N, M, V,C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
-        # x is now 4 D: N*M, C, T,V
-        # print(x.shape) -> 128, 3, 64, 25
-        #raise ValueError(x[0,:,0,:])
-        #self.plot(0, x, dim = 4, string1="afterBN")
-        
+      
 
         x = self.l1(x)
         x = self.l2(x)
