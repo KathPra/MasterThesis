444aaeca6c16a6b1151d3f0e5f68274240b204d3
K. Prasse <katharina.prasse@posteo.de>
2022-10-27 16:42:32 +0200

    Train for add. 45 epochs

--- CTR-GCN/work_dir/ntu120/csub/baseline/baseline.py
+++ CTR-GCN/work_dir/ntu120/csub/baseline/baseline.py
@@ -1,9 +1,13 @@
+from cProfile import label
 import math
-
+from multiprocessing.sharedctypes import Value
+from re import I
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def import_class(name):
     components = name.split('.')
@@ -114,7 +118,7 @@ class unit_gcn(nn.Module):
         y = self.bn(y)
         y += self.down(x)
         y = self.relu(y)
-
+        
         return y
 
 
@@ -171,9 +175,55 @@ class Model(nn.Module):
             self.drop_out = nn.Dropout(drop_out)
         else:
             self.drop_out = lambda x: x
+    def plot(self,t,x, dim, string1="Missing"):
+        for i in range(10):
+            if dim ==5:
+                x_val = x[i,0,t,:,0].cpu().detach().numpy()
+                y_val = x[i,1,t,:,0].cpu().detach().numpy()
+                z_val = x[i,2,t,:,0].cpu().detach().numpy()
+            else:
+                x_val = x[i,0,t,:].cpu().detach().numpy()
+                y_val = x[i,1,t,:].cpu().detach().numpy()
+                z_val = x[i,2,t,:].cpu().detach().numpy()
+            labels = np.array([0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,0,1,1,1,1]) #0 for spine, 1 for arms incl. shoulder, 2 for legs incl. hips
+            label_dict = {0:"Spine", 1:"Arm", 2:"Leg"}
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            for g in np.unique(labels):
+                j = np.where(labels == g)
+                ax.scatter(x_val[j], y_val[j], z_val[j], label=label_dict[g])
+ 
+            ax.set_xlim(-1,1)
+            ax.set_ylim(-1,1)
+            ax.set_zlim(-1,1)
+            ax.set_xticks([-1,-0.5,0,0.5,1])
+            ax.set_yticks([-1,-0.5,0,0.5,1])
+            ax.set_zticks([-1,-0.5,0,0.5,1])
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.set_zlabel("z")
+            ax.legend()
+            #ax.legend(labels, ["Spine","Spine","Spine","Spine","Arm","Arm","Arm","Arm","Arm","Arm","Arm","Arm","Leg","Leg","Leg","Leg","Leg","Leg","Leg","Leg","Spine","Arm","Arm","Arm"])
+            plt.savefig(f"vis/{i}/{t}_{string1}.png")
+            plt.close()
 
     def forward(self, x):
         N, C, T, V, M = x.size()
+
+        # Plot
+        #self.plot(0, x, dim = 5, string1="beforeBN")
+        #self.plot(32, x, dim = 5, string1="beforeBN")
+
+        #min_1,_ = torch.min(x, dim = 1)
+        min_1 = torch.min(x)
+    
+        mean_1 = torch.mean(x).item()
+        max_1 = torch.max(x).item()
+        check1 =  torch.sum(x[:,:,:,1,:]).item()
+
+        #raise ValueError("done")
+        
         # print(x.shape) -> 64, 3, 64, 25, 2
         x = x.permute(0, 4, 3, 1, 2).contiguous().view(N, M * V * C, T)
         # order is now N,(M,V,C),T
@@ -183,6 +233,17 @@ class Model(nn.Module):
         x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
         # x is now 4 D: N*M, C, T,V
         # print(x.shape) -> 128, 3, 64, 25
+        #raise ValueError(x[0,:,0,:])
+        #self.plot(0, x, dim = 4, string1="afterBN")
+
+        min_2 = torch.min(x).item()
+        mean_2 = torch.mean(x).item()
+        max_2 = torch.max(x).item()
+
+        #raise ValueError(x[0,:,0,0])
+        #raise ValueError("before BN", min_1, max_1, mean_1, check1,"after BN", min_2, max_2, mean_2, torch.sum(x[:,:,:,1]).item())
+        #raise ValueError(torch.min(x), torch.max(x))
+
         x = self.l1(x)
         x = self.l2(x)
         x = self.l3(x)
@@ -193,11 +254,11 @@ class Model(nn.Module):
         x = self.l8(x)
         x = self.l9(x)
         x = self.l10(x)
-
+        
         # N*M,C,T,V
         c_new = x.size(1)
         x = x.view(N, M, c_new, -1)
         x = x.mean(3).mean(1)
         x = self.drop_out(x)
-
+   
         return self.fc(x)
