407059caa09b67a04c0b933545721dd1a1c16f55
K. Prasse <katharina.prasse@posteo.de>
2022-11-07 15:39:33 +0100

    Class comparison coordinate systems.

--- CTR-GCN/vis/class_comp_LSHT.py
+++ CTR-GCN/vis/class_comp_LSHT.py
@@ -60,15 +60,48 @@ def plot_acc(acc_list, acc_label, save_name):
     acc_count = 0
     for i in acc_list:
         epoch = np.arange(0,120,1)
-        plt.plot(epoch, i, label =acc_label[acc_count])
+        plt.bar(epoch + 0.25*acc_count, i, label =acc_label[acc_count], width=0.25)
         acc_count+=1 
     plt.title(f'Class accuracy comparison')
     plt.xlabel('Class')
+    plt.xlim(left=-0.25, right=120)
     plt.ylabel('Accuracy')
-    plt.legend( loc='lower left') #bbox_to_anchor=(0.05, 0.0),
+    plt.legend(loc='lower right')
     plt.tight_layout()
     plt.savefig(f"/ceph/lprasse/MasterThesis/CTR-GCN/vis/{save_name}_class_accuracy.png")
     plt.close()
+
+def plot_acc_extr(acc_list, acc_label, save_name):
+    # Plot accuracy
+    acc_count = 0
+    for i in acc_list:
+        i = i[69:90]
+        epoch = np.arange(70,91,1)
+        plt.bar(epoch + 0.25*acc_count, i, label =acc_label[acc_count], width=0.25)
+        acc_count+=1 
+    plt.title(f'Class accuracy comparison')
+    plt.xlabel('Class')
+    #plt.xlim(left=70, right=90)
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower right')
+    plt.tight_layout()
+    plt.savefig(f"/ceph/lprasse/MasterThesis/CTR-GCN/vis/{save_name}_class_accuracy_extract2.png")
+    plt.close()
+
+
+def model_comp(acc1, acc2, class_dict,save_name):
+    diff1 = acc2-acc1
+    diff1 = np.around(diff1, decimals = 3)
+    diff1 = zip(class_dict, diff1, acc1, acc2)
+    diff1 = sorted(diff1, key = lambda diff1: diff1[1])
+
+    f = open("./vis/class_comp_trans.txt", "a")
+    f.write(save_name+": first is better"+"\n")
+    f.writelines(str(item)+"\n" for item in diff1 )
+    f.write(save_name+": second is better"+"\n")
+    f.write("\n")
+    #f.writelines(str(item) for item in diff2 )
+    f.close()
 
 # ## Baseline (CSET)
 # print("Baseline CSET")
@@ -82,32 +115,20 @@ acc2, conf2 = load_data("csub/baseline/epoch57")
 plot_CM(conf2, "baseline_CSUB")
 Baseline_t5, Baseline_l5, Baseline = top_flop(acc2, ntu120_class)
 
-## Baseline_imp
-print("Baseline_imp CSUB")
-acc3, conf3 = load_data("csub/baseline_imp/epoch61")
-plot_CM(conf3, "baseline_imp_CSUB")
-BaselineIMP_t5, BaselineIMP_l5,BaselineIMP = top_flop(acc3, ntu120_class)
-
-## spherical_coord
-print("Local_Sphercoord BN, C, T")
-acc4, conf4 = load_data("csub/local_spher_coord/epoch58")
-plot_CM(conf4, "spherical_coord_CSUB")
-SphericalCoord_t5, SphericalCoord_l5, SphericalCoord = top_flop(acc4, ntu120_class)
-
 ## local_sphercoord
 print("Local_Sphercoord T, BN, C")
 acc5, conf5 = load_data("csub/local_SHT1b/epoch39")
 plot_CM(conf5, "local_spherical_coord_CSUB")
 SphericalCoord1_t5, SphericalCoord1_l5,SphericalCoord1 = top_flop(acc5, ntu120_class)
 
-# ## local_sphercoord w/o norm
-# print("Local_Sphercoord CSUB 2")
-# acc6, conf6 = load_data("csub/local_SHT1/epoch39")
-# plot_CM(conf6, "local_spherical_coord_unnormed_CSUB")
-# SphericalCoord2_t5, SphericalCoord2_l5, SphericalCoord2 = top_flop(acc6, ntu120_class)
+## global spherical coordinates
+print("Local_Sphercoord CSUB 2")
+acc6, conf6 = load_data("csub/spherical_coord2/epoch61")
+plot_CM(conf6, "global_spherical_coord_best_CSUB")
+SphericalCoord2_t5, SphericalCoord2_l5, SphericalCoord2 = top_flop(acc6, ntu120_class)
 
 
-results = [Baseline, BaselineIMP, SphericalCoord, SphericalCoord1]
+results = [Baseline, SphericalCoord2, SphericalCoord1]
 
 # for i in results:
 #     print("start")
@@ -120,32 +141,39 @@ results = [Baseline, BaselineIMP, SphericalCoord, SphericalCoord1]
 
 ## Plot accuracies of models
 label_list =  ["Baseline","Baseline (Cent.)","Local Spher. Coord (BN,C,T)", "Local Spher. Coord (T,BN,C)" ]
-label_list1 =  ["Spher. Coord","Local Spher. Coord", "Local Spher. Coord 2" ]
-label_list2 =  ["Baseline","Baseline (Cent.)" ]
+label_list1 =  ["Local Spher. Coord (BN, C, T)", "Local Spher. Coord (T, BN, C)" ]
+label_list2 =  ["Baseline","Global Spherical Coord.", "Local Spherical Coord." ]
 
-plot_acc([acc2, acc3, acc4, acc5], label_list, "Local_Spher_Coord")
-plot_acc([acc4, acc5], label_list1, "Local_Spher_Coord_excl.")
-#plot_acc([acc2, acc3], label_list2, "Baseline")
+# plot_acc([acc2, acc3, acc4, acc5], label_list, "Local_Spher_Coord")
+# plot_acc_extr([acc2, acc3, acc4, acc5], label_list, "Local_Spher_Coord")
+# plot_acc([acc4, acc5], label_list1, "Local_Spher_Coord_excl.")
+# plot_acc_extr([acc4, acc5], label_list1, "Local_Spher_Coord_excl.")
+# plot_acc([acc3, acc6, acc5], label_list2, "Comp._Spher_Coord_excl.")
+#plot_acc_extr([acc2, acc6, acc5], label_list2, "Comp._Local_Spher_Coord_excl.")
+# model_comp(acc2, acc5, ntu120_class, "Baseline_LocalSH")
+# model_comp(acc2, acc6, ntu120_class, "Baseline_GlobalSH")
 
-for i in ['A65. tennis bat swing.', "A66. juggling table tennis balls.",'A67. hush (quite).', 'A68. flick hair.', "A69. thumb up.",'A70. thumb down.','A71. make ok sign.']:
+
+
+for i in ['A23. hand waving.', "A34. rub two hands together.",'A66. juggling table tennis balls.']:
     print(i)
     for j in results:
         print(j[i])
 
-for i in ['A72. make victory sign.','A73. staple book.', 'A74. counting money.' ,'A75. cutting nails.','A76. cutting paper (using scissors).', 'A77. snapping fingers.']:
-    print(i)
-    for j in results:
-        print(j[i])
-
-
-# for i in ['A78. open bottle.','A79. sniff (smell).']:
+# for i in ['A72. make victory sign.','A73. staple book.', 'A74. counting money.' ,'A75. cutting nails.','A76. cutting paper (using scissors).', 'A77. snapping fingers.']:
 #     print(i)
 #     for j in results:
 #         print(j[i])
 
 
+# # for i in ['A78. open bottle.','A79. sniff (smell).']:
+# #     print(i)
+# #     for j in results:
+# #         print(j[i])
 
-# for i in ["A36. shake head.","A37. wipe face.","A38. salute.","A39. put the palms together.","A40. cross hands in front (say stop).","A41. sneeze/cough.","A42. staggering.","A43. falling."]:
-#     print(i)
-#     for j in results:
-#         print(j[i])
+
+
+# # for i in ["A36. shake head.","A37. wipe face.","A38. salute.","A39. put the palms together.","A40. cross hands in front (say stop).","A41. sneeze/cough.","A42. staggering.","A43. falling."]:
+# #     print(i)
+# #     for j in results:
+# #         print(j[i])
